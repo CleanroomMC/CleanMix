71e49e17160ec422b659f2ce3e0acb4a2a48b3d9
Rongmario <rongmario@proton.me>
2026-06-24 13:47:59 +0100

    Move Srg2McpRemapper to the appropriate sourceset

--- src/main/java/org/spongepowered/asm/obfuscation/mapping/mcp/Srg2McpRemapper.java
+++ src/main/java/org/spongepowered/asm/obfuscation/mapping/mcp/Srg2McpRemapper.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.asm.obfuscation.mapping.remap;
+package org.spongepowered.asm.obfuscation.mapping.mcp;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
